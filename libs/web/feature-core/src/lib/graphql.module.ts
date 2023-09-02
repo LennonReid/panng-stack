0e2af10b87bc19ea791fb0616ea3ec332ec1e728
Lennon Reid <lenonrade@163.com>
2023-09-03 02:24:58 +0800

    fix(deps): fix the headers type error in the method createApollo and tell scripts need to use dotenv cli

--- libs/web/feature-core/src/lib/graphql.module.ts
+++ libs/web/feature-core/src/lib/graphql.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core'
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core'
+import { ApolloClientOptions, createHttpLink, InMemoryCache } from '@apollo/client/core'
 import { APOLLO_OPTIONS } from 'apollo-angular'
 import { HttpLink } from 'apollo-angular/http'
 import { environment } from '../environments/environment'
+import { setContext } from '@apollo/client/link/context'
 
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-  // This should come from AuthService
-  const token = localStorage.getItem('WEB_AUTH_TOKEN')
-  const headers: any = {
-    Authorization: token ? `Bearer ${token}` : null,
-  }
+export function createApollo(): ApolloClientOptions<any> {
+  const httpLink = createHttpLink({
+    uri: environment.graphqlUri,
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('WEB_AUTH_TOKEN')
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
   return {
-    link: httpLink.create({
-      uri: environment.graphqlUri,
-      headers,
-    }),
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   }
 }
