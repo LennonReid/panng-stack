b7353472b6076829cc010e576e5fdbfad1baf2bb
Lennon Reid <lenonrade@163.com>
2023-09-03 17:18:05 +0800

    chore(deps): update to NX@13.1.4 and migrate related dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/api',
-    '<rootDir>/libs/api/data-access',
-    '<rootDir>/libs/api/feature-core',
-    '<rootDir>/libs/api/feature-auth',
-    '<rootDir>/apps/web',
-    '<rootDir>/libs/web/data-access',
-    '<rootDir>/libs/web/feature-shell',
-    '<rootDir>/libs/web/feature-auth',
-    '<rootDir>/libs/api/feature-post',
-    '<rootDir>/libs/api/feature-comment',
-    '<rootDir>/libs/api/feature-profile',
-    '<rootDir>/libs/web/feature-core',
-    '<rootDir>/libs/web/feature-post',
-    '<rootDir>/libs/web/feature-profile',
-    '<rootDir>/libs/web/data-access-auth',
-  ],
-}
+const { getJestProjects } = require('@nrwl/jest')
+
+module.exports = { projects: getJestProjects() }
